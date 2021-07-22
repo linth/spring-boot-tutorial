1c15a10c4de3ac5d57f2a028766aa7d717d641d9
linth <zhua0404@gmail.com>
2021-07-22 10:59:25 +0800

    add create product function.

--- src/main/java/nr/crud/controller/ProductController.java
+++ src/main/java/nr/crud/controller/ProductController.java
@@ -2,18 +2,23 @@ package nr.crud.controller;
 
 import nr.crud.entity.Product;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 /**
  * 定義API的controller和路徑
@@ -37,7 +42,7 @@ public class ProductController {
         productDB.add(new Product("B0005", "SAMSUNG", 330));
     }
 
-    @GetMapping("/product/{id}")
+    @GetMapping("/productInfo/{id}")
     public Product getProduct(@PathVariable("id") String id) {
         Product product = new Product();
         product.setId(id);
@@ -48,7 +53,7 @@ public class ProductController {
 
     // 自訂回應 ResponseEntity
     // TODO: ResponseEntity concept.
-    @GetMapping("/productInfo/{id}")
+    @GetMapping("/product/{id}")
     public ResponseEntity<Product> getProductInfo(@PathVariable("id") String id) {
         // TODO: Optional, stream concept.
         Optional<Product> productOp = productDB.stream().filter(p -> p.getId().equals(id)).findFirst();
@@ -59,5 +64,33 @@ public class ProductController {
         } else {
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @PostMapping("/product")
+    // TODO: annotation
+    public ResponseEntity<Product> createProduct(@RequestBody Product request) {
+        boolean isIdDuplicated = productDB.stream().anyMatch(p -> p.getId().equals(request.getId()));
+
+        // TODO: HTTP status concept, and so on.
+        /**
+         * Reference - https://developer.mozilla.org/zh-TW/docs/Web/HTTP/Status
+         * 
+         * HTTP status: - 200: OK, 201: create, 202: accepted.
+         */
+        if (isIdDuplicated) {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+        }
+
+        Product product = new Product();
+        product.setId(request.getId());
+        product.setName(request.getName());
+        product.setPrice(request.getPrice());
+        productDB.add(product);
+
+        // TODO: URI
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(product.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).body(product);
     }
 }
